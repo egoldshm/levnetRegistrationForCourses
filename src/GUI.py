87ba118cf986c815e0e2d7cae605338ea35124f4
ardarsha <fortunnlebear@gmail.com>
2019-08-26 21:23:20 +0300

    Some style changes to gui

--- src/GUI.py
+++ src/GUI.py
@@ -12,6 +12,10 @@ import AddCourse
 ###                                                         ###
 ###############################################################
 
+PADDING_X = 20
+PADDING_Y = 5
+
+ENTRY_WIDTH = 20
 
 def login():
 	username = usernameInput.get()
@@ -27,44 +31,44 @@ def main():
 	window.title('auto register for courses')
 	row = 0
 	# username
-	usernameLabel = tk.Label(window, text = 'Username', font = 'None 12 bold', padx = 2, pady = 2)
+	usernameLabel = tk.Label(window, text = 'Username', font = 'None 12 bold', padx = PADDING_X, pady = PADDING_Y)
 	usernameLabel.grid(row = row, sticky = tk.E)
 
-	usernameInput = tk.Entry(window, font = 'None 12')
-	usernameInput.grid(row = row, column = 1)
+	usernameInput = tk.Entry(window, width = ENTRY_WIDTH, font = 'None 12', justify = 'center')
+	usernameInput.grid(row = row, column = 1, padx = PADDING_X, pady = PADDING_Y)
 	row += 1
 	# password
-	passwordLabel = tk.Label(window, text = 'Password', font = 'None 12 bold', padx = 2, pady = 2)
+	passwordLabel = tk.Label(window, text = 'Password', font = 'None 12 bold', padx = PADDING_X, pady = PADDING_Y)
 	passwordLabel.grid(row = row, sticky = tk.E)
 
-	passwordInput = tk.Entry(window, show = '•', font = 'None 12')
-	passwordInput.grid(row = row, column = 1)
+	passwordInput = tk.Entry(window, width = ENTRY_WIDTH, show = '•', font = 'None 12', justify = 'center')
+	passwordInput.grid(row = row, column = 1, padx = PADDING_X, pady = PADDING_Y)
 	row += 1
 	# number of courses
 	# course id
-	idLabel = tk.Label(window, text = 'Course ID', font = 'None 12 bold', padx = 2, pady = 2)
+	idLabel = tk.Label(window, text = 'Course ID', font = 'None 12 bold', padx = PADDING_X, pady = PADDING_Y)
 	idLabel.grid(row = row, sticky = tk.E)
 
-	idInput = tk.Entry(window, font = 'None 12')
-	idInput.grid(row = row, column = 1)
+	idInput = tk.Entry(window, width = ENTRY_WIDTH, font = 'None 12', justify = 'center')
+	idInput.grid(row = row, column = 1, padx = PADDING_X, pady = PADDING_Y)
 	row += 1
 	# group numbers
-	groupNumbersLabel = tk.Label(window, text = 'Group Numbers', font = 'None 12 bold', padx = 2, pady = 2)
+	groupNumbersLabel = tk.Label(window, text = 'Group Numbers', font = 'None 12 bold', padx = PADDING_X, pady = PADDING_Y)
 	groupNumbersLabel.grid(row = row, sticky = tk.E)
 
-	groupNumbersInput = tk.Entry(window, font = 'None 12')
-	groupNumbersInput.grid(row = row, column = 1)
+	groupNumbersInput = tk.Entry(window, width = ENTRY_WIDTH, font = 'None 12', justify = 'center')
+	groupNumbersInput.grid(row = row, column = 1, padx = PADDING_X, pady = PADDING_Y)
 	row += 1
 	# login
-	loginButton = tk.Button(window, text = 'Login', command = login, padx = 2, pady = 2)
-	loginButton.grid(row = row)
+	loginButton = tk.Button(window, text = 'Login', command = login, padx = PADDING_X, pady = PADDING_Y)
+	loginButton.grid(row = row, columnspan = 2, padx = PADDING_X, pady = PADDING_Y)
 	row += 1
 
 	# result
 	resultText = tk.StringVar(value = '')
 
-	resultLabel = tk.Label(window, textvariable = resultText, padx = 2, pady = 2)
-	resultLabel.grid(row = row)
+	resultLabel = tk.Label(window, textvariable = resultText, padx = PADDING_X, pady = PADDING_Y, fg = 'red')
+	resultLabel.grid(row = row, columnspan = 2)
 	# gui mainloop
 	window.mainloop()
 
