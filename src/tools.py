5e440c5d2c37010f21459983989e82661eee2009
ardarsha <fortunnlebear@gmail.com>
2019-08-27 02:15:13 +0300

    Added Levnet class to tools.py, to contain a session class for levnet sessions

--- src/tools.py
+++ src/tools.py
@@ -3,8 +3,12 @@ import json
 
 headers = {'Host' : 'levnet.jct.ac.il', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'}
 
+loginUrl = 'https://levnet.jct.ac.il/api/home/login.ashx?action=TryLogin'
+ScheduleStart = 'https://levnet.jct.ac.il/Student/Schedule/Start.aspx'
+BuildScheduleStart = 'https://levnet.jct.ac.il/api/student/buildSchedule.ashx?action=LoadDataForBuildScheduleStart'
 
 DebugMode = False
+verify = False
 
 
 def PrintError(message):
@@ -30,14 +34,14 @@ def toJson(x):
 
 def GET(session, url):
     '''send GET request to url and do few checks'''
-    r = session.get(url, headers = headers, verify = False)
+    r = session.get(url, headers = headers, verify = verify)
     Assert(r)
     debug(r.text)
     return r
 
 def POST(session, url, data):
     '''send a POST request to url with data and do few checks'''
-    r = session.post(url, json = data,  headers = headers, verify = False)
+    r = session.post(url, json = data,  headers = headers, verify = verify)
     Assert(r)
     debug(r.text)
     return r
@@ -58,3 +62,44 @@ def getIdOfGroups(json, groupNumbers):
     ids = list(map(lambda j: j["id"], courseWithRightId))
     return {"actualCourseId":id,"selectedGroups":ids}
 
+
+class Levnet:
+    class Session(requests.Session):
+
+        def __init__(self, username, password):
+            requests.Session.__init__(self)
+            self.username = username
+            self.password = password
+
+        def GET(self, url):
+            r = self.get(url, headers = headers, verify = verify)
+            Assert(r)
+            debug(r.text)
+            return r
+
+        def POST(self, url, json):
+            r = self.post(url, json = json, headers = headers, verify = verify)
+            Assert(r)
+            debug(r.text)
+            return r
+
+        def Login(self):
+            r = self.POST(loginUrl, json = { 'username' : self.username, 'password' : self.password })
+            return toJson(r)['success']
+
+        def OpenSchedule(self):
+            self.GET(ScheduleStart)
+            r = self.POST(BuildScheduleStart, json = { 'username' : self.username, 'password' : self.password })
+
+            whatOpen = toJson(r)["semestersScheduleCreation"]
+
+            if whatOpen == []:
+                return False
+
+            year = whatOpen[0]["academicYearId"]
+            semester = whatOpen[0]["semesterId"]
+            return {"academicYear":year,"semester":semester}
+
+        def ScheduleWarnings(self):
+            r = self.POST(LoadRegWarnings, json = '')
+            return toJson(r)["regWarnings"]
