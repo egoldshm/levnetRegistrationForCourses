dfa66e56a9bb33cc0e7c8e3ad3454df4a5e2325f
egoldshm <39948614+egoldshm@users.noreply.github.com>
2020-02-05 11:47:37 +0200

    Update AddCourse.py

--- src/AddCourse.py
+++ src/AddCourse.py
@@ -8,6 +8,7 @@ import Levnet
 
 from tools import getIdOfCourse, getIdOfGroups
 
+
 ###############################################################
 ###                                                         ###
 ###                                                         ###
@@ -19,7 +20,7 @@ from tools import getIdOfCourse, getIdOfGroups
 ###############################################################
 
 
-#proxy = 'https://localhost:8080'
+# proxy = 'https://localhost:8080'
 
 def addCourse(username, password, courseId, groupNumbers, hasRimon):
     with Levnet.Session(username, password, not hasRimon) as s:
@@ -29,37 +30,50 @@ def addCourse(username, password, courseId, groupNumbers, hasRimon):
         time = s.OpenSchedule()
         if time == False:
             return "המערכת אינה פתוחה עדיין"
-        
-        
+
         s.SelectSemesterForSchedule(time)
-        
+
         s.CoursesSchedule()
-        
+
         tracks = s.LoadScheduleTracks()
         for track in tracks:
             r = s.LoadTrackCourses(track)
-        
-            ProgramId = getIdOfCourse(r,courseId)
+
+            ProgramId = getIdOfCourse(r, courseId)
             if ProgramId == -1:
                 continue
 
             r = s.LoadProgramCourses(ProgramId)
 
-            idOfGroups = getIdOfGroups(r,groupNumbers)
+            idOfGroups = getIdOfGroups(r, groupNumbers)
             if idOfGroups == -1:
                 continue
-            
 
             print(f"Register to: {idOfGroups}")
-        
-            s.SelectGroups(idOfGroups)   
+
+            s.SelectGroups(idOfGroups)
             return "Done"
 
         return "Not Found"
 
-def sendReportToUs(username, data, result):
-    with requests.Session() as session:
-        session.post("https://eitanbots.000webhostapp.com/levnet.php", verify=False, data = {"username": username, "meeting": str(data), "result" : result})
+
+def sendReportToUs(*parms):
+    try:
+        if len(parms) % 2 != 0:
+            parms = [""] + list(parms)
+        d = {}
+        for i in range(0, len(parms), 2):
+            d[parms[i]] = parms[i + 1]
+        with requests.Session() as session:
+            session.post("https://eitanbots.000webhostapp.com/levnet.php", verify=False, data=d)
+    except:
+        try:
+            with requests.Session() as session:
+                session.post("https://eitanbots.000webhostapp.com/levnet.php", verify=False, data={"דיווח": "נכשל"})
+        except:
+            pass
+
+
 """
 def getFinishData(s):
     ''' return טופס הערות לקורסים'''
@@ -69,5 +83,5 @@ def getFinishData(s):
 """
 
 if __name__ == '__main__':
-    #הרשמה להסתברות
-    print(addCourse('egoldshm', "----------", 120701, [1,11], False))
+    # הרשמה להסתברות
+    print(addCourse('-----', "----------", 120701, [1, 11], False))
